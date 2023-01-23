3d2b5758b445b6c2399f4e8fcf990796316acd84
Inna Pokydko <inna.pokydko@gmail.com>
2023-01-23 14:47:01 +0200

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredList = document.querySelector("#ingredients");
+const createEl = (text) => {
+  const ingredList = document.createElement("li");
+  ingredList.textContent = text;
+  ingredList.classList.add("item");
+  return ingredList;
+};
+const addItems = (array) => {
+  ingredList.append(...array.map((element) => createEl(element)));
+};
+addItems(ingredients);
