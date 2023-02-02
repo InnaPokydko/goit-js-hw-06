feb73b59b4e584e889710acd08b6c4fd00504d06
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-02 11:26:41 +0200

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,19 +1,19 @@
 
-const form = document.querySelector(".form");
+const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-//   const {
-//     elements: { login, password }
-//   } = event.currentTarget;
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
 
-//   if (login.value === "" || password.value === "") {
-//     return console.log("Please fill in all the fields!");
-//   }
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
 
-//   console.log(`Login: ${login.value}, Password: ${password.value}`);
-//   event.currentTarget.reset();
-// }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
 
