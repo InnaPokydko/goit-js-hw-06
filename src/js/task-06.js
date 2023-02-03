4eacafc8ef307d25acd09723bfc864d8624bd8d1
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-03 15:00:11 +0200

    task6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,8 +3,8 @@ const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener('blur', () => {
     if (textInput.value.length === Number(textInput.dataset.length)) {
-        textInput.classList.remove(".invalid");
-        return textInput.classList.add(".valid");
-    }
-    return textInput.classList.add(".invalid");
-})
+        textInput.classList.remove("invalid");
+        return textInput.classList.add("valid");
+    } else
+    return textInput.classList.add("invalid");
+});
