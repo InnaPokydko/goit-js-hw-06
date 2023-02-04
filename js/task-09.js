5a6ec80e2ad0f0ce8c61f26da83933bc5dc58dcc
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-04 13:37:47 +0200

    task9 fix

--- js/task-09.js
+++ js/task-09.js
@@ -2,15 +2,15 @@
 const buttonChangeColor = document.querySelector(".change-color")
 const bodyChangeColor = document.querySelector(".color")
 
-function getRandomHexColor() {
+const color = function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
 buttonChangeColor.addEventListener("click", () => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  bodyChangeColor.textContent = document.body.style.backgroundColor;
+  document.body.style.backgroundColor = color();
+  bodyChangeColor.textContent = color();
 });
 
 
