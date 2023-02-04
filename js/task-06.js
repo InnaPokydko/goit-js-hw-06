f452d9928d984d54ed7d1af8b6643ab6d1ec02e6
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-04 13:08:29 +0200

    task6 fix

--- js/task-06.js
+++ js/task-06.js
@@ -3,6 +3,7 @@ const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener('blur', () => {
     if (textInput.value.trim().length === Number(textInput.dataset.length)) {
+         textInput.classList.remove("invalid");
         return textInput.classList.add("valid");
     } else
     return textInput.classList.replace("valid", "invalid");
